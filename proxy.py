55b525fa54161b938e21083711f1c8ed3ec138bf
hofstede-matheus <hofstede.matheus@gmail.com>
2022-11-28 17:38:29 -0300

    build deposit flow

--- proxy.py
+++ proxy.py
@@ -1,15 +1,41 @@
-class Proxy:
-  mylist = ["apple", "banana", "cherry"]
-  # def __init__(self):
+import os
+import socket
+import time
 
+HOST = 'localhost'
+PORT = 5959
+SEPARATOR = "|"
+BUFFER_SIZE = 1024
+
+
+class Proxy:
   def get_file_list():
-    print("Getting file list...")
+    print("Lista de arquivos:")
+    subfolders = [ f.path.split('/')[1] for f in os.scandir("dump") if f.is_dir() ]
+    print(subfolders)
 
   def deposit_file(nome_do_arquivo: str, nivel_de_tolerancia: int):
     print("Deposita...")
+    app_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    app_socket.connect((HOST, PORT))
+
+    action = "TYPE_DEPOSIT"
+    filesize = os.path.getsize(nome_do_arquivo)
+
+    app_socket.send(f"{action}{SEPARATOR}{nome_do_arquivo}{SEPARATOR}{filesize}{SEPARATOR}{nivel_de_tolerancia}".encode())
+    message, _ = app_socket.recvfrom(BUFFER_SIZE)
+
+    print("arquivos já existe?", message.decode())
+    time.sleep(1)
+
+    with open(nome_do_arquivo, "rb") as f:
+        while True:
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+              break
+            app_socket.sendall(bytes_read)
+
+    app_socket.close()
 
   def recover_file(nome_do_arquivo: str):
     print("Recupera...")
-
-  def __private_method():
-    print("Private method")
---
+++ server.py
@@ -0,0 +1,64 @@
+import socket
+from pathlib import Path
+import os
+import shutil
+import os.path
+import time
+
+HOST = 'localhost'
+PORT = 5959
+BUFFER_SIZE = 1024
+SEPARATOR = "|"
+
+def start():
+  app_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPv4, TCP
+  app_socket.bind((HOST, PORT))
+  app_socket.listen()
+
+  print("Aguardando conexão do cliente...")
+  conn, address = app_socket.accept()
+
+  print('Conectado com:', address)
+
+  data = conn.recv(BUFFER_SIZE)
+  decoded_data = data.decode()
+
+  action, filename, filesize, replicas = decoded_data.split(SEPARATOR)
+  print("Recebido:", action, filename, filesize, replicas)
+
+  file_path = Path(f"dump/{filename}/{filename}_1")
+
+  fileExists = os.path.isfile(file_path)
+
+  if fileExists:
+    data = str.encode('ALREADY_EXISTS')
+    conn.sendall(data)
+    print("ALREADY_EXISTS")
+
+    for i in range(2, int(replicas)+1):
+      shutil.copy(file_path, Path(f"dump/{filename}/{filename}_{i}"))
+  else:
+    data = str.encode('NOT_EXISTS')
+    conn.sendall(data)
+    print("NOT_EXISTS")
+
+    os.makedirs(f"dump/{filename}/", exist_ok=True)
+
+    with open(file_path, "wb") as f:
+        while True:
+            bytes_read = conn.recv(BUFFER_SIZE)
+            print("\n","CHUNK:", bytes_read)
+            if not bytes_read:    
+                break
+            f.write(bytes_read)
+    # replicate
+    for i in range(2, int(replicas)+1):
+        shutil.copy(file_path, Path(f"dump/{filename}/{filename}_{i}"))
+
+
+  conn.close()
+  app_socket.close()
+  time.sleep(1)
+  start()
+
+start()
